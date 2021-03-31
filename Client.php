573d0b5ac517219c7f3545df6dd969c2e22556f9
Nikita Zarubin <n.zarubin@itcomgk.ru>
2021-03-31 17:47:37 +0300

    init

--- Client.php
+++ Client.php
@@ -2,23 +2,66 @@
 
 namespace nikserg\EyeApi;
 
+use nikserg\EyeApi\model\Task;
+
 class Client
 {
     private $baseUrl;
+    private $client;
     const ACTION_CROP = 'crop';
 
     public function __construct($baseUrl)
     {
         $this->baseUrl = $baseUrl;
+        $this->client = new \GuzzleHttp\Client();
+    }
+    private function getUrl($action) {
+        return $this->baseUrl . '/' . $action;
     }
 
+    /**
+     * @param $filename
+     * @return Task
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
     public function crop($filename)
     {
-
+        $response = $this->client->request('POST', $this->getUrl(self::ACTION_CROP), [
+            'multipart' => [
+                [
+                    'name' => 'file',
+                    'contents' => file_get_contents($filename),
+                    'filename' => basename($filename)
+                ]
+            ]
+        ]);
+        return $this->responseToTask($response);
     }
 
-    public function check($id)
+    /**
+     * @param $action
+     * @param $id
+     * @return Task
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function check($action, $id)
+    {
+        $response = $this->client->get($this->getUrl($action).'/'.$id);
+        return $this->responseToTask($response);
+    }
+
+    private function responseToTask($response)
     {
 
+        $response = json_decode($response, true);
+
+        $task = new Task();
+        $task->id = $response['id'];
+        $task->status = $response['status'];
+        if (isset($response['resultUrl'])) {
+            $task->resultUrl = $response['resultUrl'];
+        }
+
+        return $task;
     }
 }
--- model/Task.php
+++ model/Task.php
@@ -3,4 +3,11 @@ namespace nikserg\EyeApi\model;
 
 class Task {
 
+    const STATUS_NEW = 'new';
+    const STATUS_DONE = 'done';
+
+
+    public $id;
+    public $status;
+    public $resultUrl;
 }
